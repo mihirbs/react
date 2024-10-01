9a0a399ddde0d3084e137bc2a25a718b0743d755
mihirbs <mihirsolanki2704@gmail.com>
2024-10-01 17:12:43 +0530

    new files are added

---
+++ ev-website/src/App.jsx
@@ -0,0 +1,17 @@
+
+ const App = () => {
+  let heroData = [
+    {text1:"Dive into ",text2:"What you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give in to ",text2:"your passions"},
+  ]
+  const [heroCount,setHeroCount] = useState(2);
+  const [playStatus,setPlayStatus]= useState(false);
+  return (
+    <div>
+      <Background playStatus=/{playStatus} heroCount={heroCount}/>
+    </div>
+  )
+}
+
+export default App
